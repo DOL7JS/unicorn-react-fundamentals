af675b174dae551617b1d7cf07c6493a84ecb4ab
Petr Dolejs <petr.dolejs.1@unicorn.cz>
2024-08-06 07:39:45 +0200

    Cookbook - implementing card of recipes

--- cookbook/client/src/components/Header.js
+++ cookbook/client/src/components/Header.js
@@ -1,6 +1,5 @@
 import headerStyle from "../css/Header.css"
-import Icon from "@mdi/react";
-import { mdiFoodForkDrink } from '@mdi/js';
+
 function Header(props) {
     return (<>
         <h1 className={headerStyle.header}>
